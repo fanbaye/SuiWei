5a6911c5606292aa2a647bd22b7866b5f13c0e00
lucas <share_lu_gz@yahoo.com.cn>
2013-05-15 23:17:35 +0800

    类别选择视图控制器的UI优化

---
+++ SuiWei/CategoryCell.h
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.h
+//  SuiWei
+//
+//  Created by lucas on 5/15/13.
+//  Copyright (c) 2013 lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryCell : UITableViewCell
+
+@end
